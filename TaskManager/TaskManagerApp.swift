7ffd5d17a3fa9963fa69e50f296c6dc94df860b9
Sooryasanand <sooryanarayanan.com@gmail.com>
2025-09-01 11:16:22 +1000

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Soorya Narayanan Sanand on 1/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
